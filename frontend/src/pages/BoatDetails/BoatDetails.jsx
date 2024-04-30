0e875212e546884093e3524685775ead921fda9a
AdrSpeer <adn.speer@gmail.com>
2024-04-30 13:02:16 +0200

    added Reservation Card for BoatDetails

--- frontend/src/pages/BoatDetails/BoatDetails.jsx
+++ frontend/src/pages/BoatDetails/BoatDetails.jsx
@@ -5,9 +5,23 @@ import { useParams } from "react-router-dom";
 import { backendURL } from "../../api/api";
 import DeleteBoat from "../../components/DeleteBoat/DeleteBoat";
 import EditBoat from "../../components/EditBoat/EditBoat";
+import { fetchAllReservationsContext } from "../../context/Context";
+import ReservationCard from "../../components/ReservationCard/ReservationCard";
 const BoatDetails = () => {
   const [boatDetails, setBoatDetails] = useState([]);
+  const [boatReservations, setBoatReservations] = useState([]);
   const { boatId } = useParams();
+  const { allReservations, setAllReservations } = useContext(
+    fetchAllReservationsContext
+  );
+
+  useEffect(() => {
+    const filterReservations = allReservations.filter(
+      (reservation) => reservation.boatId === boatDetails._id
+    );
+    setBoatReservations(filterReservations);
+  }, [boatDetails]);
+  console.log(boatReservations);
 
   useEffect(() => {
     fetch(`${backendURL}/api/v1/boats/${boatId}`)
@@ -25,6 +39,10 @@ const BoatDetails = () => {
       <p>Seriennummer: {boatDetails.serialNumber}</p>
       <DeleteBoat boatId={boatDetails._id} />
       <EditBoat boat={boatDetails} />
+      <h2>Reservierungen</h2>
+      {boatReservations?.map((item) => (
+        <ReservationCard key={item._id} reservation={item} />
+      ))}
     </section>
 
     // Show all reservations for boatId
