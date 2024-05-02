6ed761d4e085ae74542ee596d924594f774cb89e
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-05-02 08:42:57 +0200

    confirm delete box done

--- frontend/src/components/DeleteBoat/DeleteBoat.jsx
+++ frontend/src/components/DeleteBoat/DeleteBoat.jsx
@@ -15,8 +15,11 @@ const DeleteBoat = ({ boatId }) => {
   // state for error message to user
   const [error, setError] = useState("");
 
-  // function to delete a boat - only if there are no reservations for the boat
-  const deleteBoat = () => {
+  // state to toggle delete-confirm-box
+  const [toggleDelete, setToggleDelete] = useState(false);
+
+  // function to check, if you really want to delete this boat - only if there are no reservations for the boat
+  const checkDeleteBoat = () => {
     const filterReservedBoats = allReservations.filter(
       (res) => res.boatId === boatId
     );
@@ -27,6 +30,11 @@ const DeleteBoat = ({ boatId }) => {
       );
     }
 
+    setToggleDelete(true);
+  };
+
+  // function to delete a boat (after you confirmed it via toggled div-box-button)
+  const deleteBoat = () => {
     fetch(`${backendURL}/api/v1/boats/${boatId}`, { method: "DELETE" })
       .then((res) => res.json())
       .then((data) =>
@@ -38,7 +46,7 @@ const DeleteBoat = ({ boatId }) => {
   return (
     <>
       <svg
-        onClick={deleteBoat}
+        onClick={checkDeleteBoat}
         xmlns="http://www.w3.org/2000/svg"
         width={"15px"}
         viewBox="0 0 448 512"
@@ -46,6 +54,12 @@ const DeleteBoat = ({ boatId }) => {
         <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
       </svg>
       {error.length > 0 ? <p className="error">{error}</p> : ""}
+
+      <div className={toggleDelete ? "check-delete show" : "check-delete hide"}>
+        <p>Willst du dieses Boot wirklich löschen?</p>
+        <button onClick={deleteBoat}>ja</button>
+        <button onClick={() => setToggleDelete(false)}>nein</button>
+      </div>
     </>
   );
 };
--- frontend/src/components/DeleteReservation/DeleteReservation.jsx
+++ frontend/src/components/DeleteReservation/DeleteReservation.jsx
@@ -2,9 +2,14 @@ import { backendURL } from "../../api/api";
 import { fetchAllReservationsContext } from "../../context/Context";
 import { useContext, useState } from "react";
 const DeleteReservation = ({ reservationId }) => {
+  // global fetch for all reservations
   const { allReservations, setAllReservations } = useContext(
     fetchAllReservationsContext
   );
+
+  // state to toggle delete-confirm-box
+  const [toggleDelete, setToggleDelete] = useState(false);
+
   const deleteReservation = () => {
     fetch(`${backendURL}/api/v1/reservations/${reservationId}`, {
       method: "DELETE",
@@ -19,14 +24,22 @@ const DeleteReservation = ({ reservationId }) => {
   };
 
   return (
-    <svg
-      onClick={deleteReservation}
-      xmlns="http://www.w3.org/2000/svg"
-      width={"18px"}
-      viewBox="0 0 448 512"
-    >
-      <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
-    </svg>
+    <>
+      <svg
+        onClick={() => setToggleDelete(true)}
+        xmlns="http://www.w3.org/2000/svg"
+        width={"18px"}
+        viewBox="0 0 448 512"
+      >
+        <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+      </svg>
+
+      <div className={toggleDelete ? "check-delete show" : "check-delete hide"}>
+        <p>Willst du diese Reservierung wirklich löschen?</p>
+        <button onClick={deleteReservation}>ja</button>
+        <button onClick={() => setToggleDelete(false)}>nein</button>
+      </div>
+    </>
   );
 };
 
